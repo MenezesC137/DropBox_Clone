a88fd8e2a8386fb2bc5d734c242c60702b046537
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-15 10:58:04 -0300

    Modal Bar

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
-var formidable = require('formidable');
-
+var fomidable = require('formidable');
+ 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.post('/upload', (req, res) => {
-
-  let form = new formidable.IncomingForm({
-    uploadDir: './upload',
-    keepExtensions: true
-  })
-
-  form.parse(req, (err, fields, files) => {
-    res.json({
-      files
-    })
-  })
-})
-
+ 
+router.post('/upload',(req,res)=>{
+   
+    let form = new fomidable.IncomingForm({
+        uploadDir:'./upload',
+        KeepExtensions:true // para manter a extensão 
+    });
+ 
+    form.parse(req,(err, fields,files)=>{
+ 
+      res.json({
+        files
+      });
+ 
+    });
+ 
+});
+ 
 module.exports = router;
