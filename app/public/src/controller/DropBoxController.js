b2ca38ebcf2ee232d0a90b6f9a32f428aea1865f
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-16 09:56:56 -0300

    Renomear Arquivo

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -49,6 +49,22 @@ class DropBoxController {
 
     initEvents(){
  
+        this.btnRename.addEventListener('click', e=> {
+
+            let li = this.getSelection()[0];
+
+            let file = JSON.parse(li.dataset.file);
+
+            let name = prompt("Renomear o arquivo: ", file.name)
+
+            if (name) {
+
+                file.name = name;
+
+                this.getFirebaseRef().child(li.dataset.key).ser(file);
+           
+            }
+        })
         this.listFilesEl.addEventListener('selectionchange', e =>{
             
             switch (this.getSelection().lenght) {
@@ -383,6 +399,7 @@ class DropBoxController {
         let li = document.createElement('li')
 
         li.dataset.key = key;
+        li.dataset.file = JSON.stringify(file);
 
         li.innerHTML = `
                 ${this.getFileIconView(file)}
@@ -463,7 +480,7 @@ class DropBoxController {
             li.classList.toggle('selected')
 
             this.listFilesEl.dispatchEvent(this.onselectionchange);
-            
+
         })
     }
 }
