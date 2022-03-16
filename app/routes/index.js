4e1606c11d6b50b1fc4176b207a74c30b48bc047
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-16 12:19:18 -0300

    Abrindo arquivos

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,6 +8,37 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
  
+router.get('/file', (req, res) =>{
+
+  let path = './' + req.query.path;
+  
+  if (fs.existsSysnc(path)) {
+
+    fs.readFile(path, (err, data) => {
+
+      if (err){
+
+        console.error(err);
+        res.status(400).json({
+          error: err
+        })
+      } else {
+
+        res.status(200).end(data);
+
+      }
+    })
+  } else {
+
+    res.status(404).json({
+      error:'File not found.'
+
+    })
+  }
+
+});
+
+//Rota Delete
 router.delete('/file', (req, res)=> {
 
   let form = new fomidable.IncomingForm({
@@ -33,9 +64,15 @@ router.delete('/file', (req, res)=> {
           });
         }
       });
+    } else {
+      res.status(404).json({
+        error:'File not found.'
+      })
     }
   });
 })
+
+//Rota de POST
 router.post('/upload',(req,res)=>{
    
   let form = new fomidable.IncomingForm({
